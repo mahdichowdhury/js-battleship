0476b9a73a3dab8ea65f8f1f7365074977375572
mahdichowdhury <113201272+mahdichowdhury@users.noreply.github.com>
2022-10-04 12:52:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 //initizalize variables
 let myGridSize = prompt('What size would you like your grid to be?');
-let myEnemyGridSize = prompt ('What size would ou like your enemies grid to be?');
+let enemyGridSize = prompt ('What size would ou like your enemies grid to be?');
 let myGrid = createGrid(myGridSize);
-let enemyGrid = createGrid(myEnemyGridSize);
+let enemyGrid = createGrid(enemyGridSize);
+let myShips = 3;
+let enemyShips = 3;
+
 
 printGrid(enemyGrid, true);
 printGrid(myGrid);
+
+for (let i = 1; i < 4;i++ ){
+    let x = prompt('Enter the x coordinate for your ship number' +i);
+    let y = prompt ('Enter the y coordinate for your ship number' +i);
+    placeCharacter(x, y, 'O',myGrid);
+    placeRandomCharacter("O", enemyGrid, enemyGridSize);
+    printGrid(enemyGrid, true);
+    printGrid(myGrid);
+}
+
+
 
 function createGrid(size) {
     let grid = [];
@@ -41,4 +55,19 @@ function createHeaders(size) {
     return result;
 }
 
+function placeCharacter(x, y, c, grid){
+    grid[y][x]= c;
 
+}
+
+function placeRandomCharacter(c, grid, max){
+    let didPlace = false;
+    while(!didPlace){
+        let x = getRandomInt(max);
+        let y = getRandomInt(max);
+    }
+}
+
+function getRandomInt(max){
+    return Math.floor(math.random()* math.floor(max));
+}
